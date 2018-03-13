b9eeaafc7aac199916058145af32cc847112c551
HackworthH <hackworth.ameliah@gmail.com>
2018-03-13 16:42:00 -0500

    removed comments from centered average file

--- Coding_Problems/CenteredAverage.java
+++ Coding_Problems/CenteredAverage.java
@@ -27,99 +27,25 @@ public class CenteredAverage {
 		 *5. Array has to have a length of at least 3. 
 		 * */
 		
-		//since the method has a return type of an int, it has to return an int,
-		//in this case the "centered average" of the int array.
 		
-//		int min = nums[0];
-//		int max = nums[nums.length-1];
-//		int value = 0;
 		int sum = 0;
 		int avg = 0;
-	//	int [] arr = new int[nums.length];
+	
 		
 		for(int i = 0; i<nums.length; i++){
-		//	min = nums[i];
-		//	max = nums[i];
+		
 			System.out.println(nums[i]);
 			for(int j =0; j<nums.length-1-i; j++){
 				if(nums[j] > nums[j+1]){
 					int	tmp = nums[j];
 					nums[j] = nums[j+1];
 					nums[j+1] = tmp;
-					///the bubble sort is not working
-					//it didn't work because i had nums[j-1] in the if statement
+					
 				}
-				System.out.println(Arrays.toString(nums));
+				
 			}
 			
-	/*		if(nums[i] < min ){
-				min = nums[i];	
-			}
-			
-			if(nums[i] > max){
-				max = nums[i];
-				//System.out.println(max);
-			} */
-			
-			
-		/*	if(nums[i] < nums[nums.length-1]){
-				value = nums[i];
-				sum = value + value;
-				avg = sum/nums.length;
-				System.out.println(value + " This is a test");
-				System.out.println(sum + " I hope this is right");
-				System.out.println(avg + " Is this correct?");
-				//this is not working right because it is multiplying each element
-				//of the nums array by the array length instead of adding up the
-				//sum of the total array
-			} */
-			
-	/*		if(min < i){
-			//	System.out.println(i);
-				min = nums[i];	
-			//	System.out.println(min);
-			}
-			
-			if(max > i){
-				max = nums[i];
-				
-			} */
-			
-		/*	if(nums[i]==min){
-				min = 0;
-			}
-			
-			if(nums[i]==max){
-				max = 0;
-			} */
-			
-			//I figure that if the max and min values equal zero, when the program
-			//goes to add up and divide the rest of the array elements, it won't count
-			//these extreme values.
-			
-			//Arrays are fixed once they are created; I will need to
-			//make a new array without the min and max elements.
-			
-	/*		if(min == nums[i]){
-				
-			//	nums[0] = nums[i + 1];
-			//	int[] arr = {nums[i + 1], };
-				
-			//	nums = arr;
-				
-				
-			//	nums[0] = nums[i + 1];
-			//	System.out.println(Arrays.toString(nums));
-			//	System.out.println(Arrays.toString(arr));
-				//trying to create a new array without the min and max
-				//numbers so that I can get the average without them.
-				
-				
-			//try to put array into an arraylist in order to change the length of the 
-			//elements to eliminate the largest and smallest values.  Then convert the 
-			//arraylist object back into an array.	
-				
-			} */
+	
 		}
 		//converts array into an array list:
 		ArrayList<Integer> intList = new ArrayList<Integer>();	
@@ -128,29 +54,21 @@ public class CenteredAverage {
 			
 		}
 		
-		System.out.println(intList.size());
-		
+			
 		int last = nums.length - 2;
-		//returns an out of bounds error when I use the remove method:
+		
 		intList.remove(0);
 		intList.remove(last);
-		System.out.println(intList);
-		System.out.println(intList.size());
-	//	System.out.println(min);
-	//	System.out.println(max);
 		
+			
 		for(int i : intList){
 			sum += i;		
 		}
 		
-		//System.out.println(sum);
 		
 		avg = sum/intList.size();
-		System.out.println(avg);
 		
-		//give me the average but not the center average.  Need to round it down to the
-		//nearest value in the array.  
-		
+	
 		return avg;
 		
 	}
@@ -161,7 +79,7 @@ public class CenteredAverage {
 		
 		CenteredAverage.centeredAverage(nums);
 		
-	//	System.out.println(centeredAverage(nums));
+	// expected result: 3
 
 	}
 
